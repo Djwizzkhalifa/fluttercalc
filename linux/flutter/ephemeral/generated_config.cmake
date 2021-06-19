b49ec3d365af68493f32233179b24e63e1f71604
Freddy Fernandes <64181600+Djwizzkhalifa@users.noreply.github.com>
2021-06-19 20:45:20 +0530

    Add files via upload

---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,15 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/freddyfernandes/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/freddyfernandes/Desktop/anifredcalc" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/freddyfernandes/flutter"
+  "PROJECT_DIR=/home/freddyfernandes/Desktop/anifredcalc"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/freddyfernandes/Desktop/anifredcalc/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/freddyfernandes/Desktop/anifredcalc/lib/main.dart"
+)
